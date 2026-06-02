f86ff1444188ad53d1ee5ccfee51328372f6b097
EloyPradoMora <e.prado02@ufromail.cl>
2026-06-02 16:07:55 -0400

    chore: Creacion de archivo de proyecto vacio.

---
+++ MOIKO/MOIKO/src/main/java/eloy/MOIKO/MoikoApplication.java
@@ -0,0 +1,13 @@
+package eloy.MOIKO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoikoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoikoApplication.class, args);
+	}
+
+}
---
+++ MOIKO/MOIKO/src/test/java/eloy/MOIKO/MoikoApplicationTests.java
@@ -0,0 +1,13 @@
+package eloy.MOIKO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoikoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
